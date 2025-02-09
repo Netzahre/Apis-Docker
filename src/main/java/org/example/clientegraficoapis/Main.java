9a417a2e250a5f1fa72935d44ed6ce9ba8a79b56
Netzahre <netzahre@gmail.com>
2025-02-09 16:12:35 +0000

    first commit

--- src/main/java/org/example/clientegraficoapis/Main.java
+++ src/main/java/org/example/clientegraficoapis/Main.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 
 public class Main extends Application {
     public static User currentUser;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
